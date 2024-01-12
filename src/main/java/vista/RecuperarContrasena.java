10b39ac66606efd41db63d168e07a6df259d8ed5
Javier Sanchez Gonzalez <94067907+javiersango@users.noreply.github.com>
2024-01-12 21:04:05 +0100

    12-01-24 Parking - idioma en clase funciona
    Funciona en recuperar contraseña

--- src/main/java/vista/RecuperarContrasena.java
+++ src/main/java/vista/RecuperarContrasena.java
@@ -50,9 +50,9 @@ public class RecuperarContrasena extends javax.swing.JFrame {
         jltitulo2.putClientProperty("FlatLaf.styleClass", "h1");
         jlemail.putClientProperty("FlatLaf.styleClass", "h2");
         
-         if (ingles) {
+        
             cambiarIdiomaEn();
-        }
+        
 
     }
 
